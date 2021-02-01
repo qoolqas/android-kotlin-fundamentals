62c8f914f791b673a0e9650fb7cd1f934e730431
Qoolqas <qoolqas@gmail.com>
2021-02-01 08:10:06 +0700

    Attach observers to the LiveData objects

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -24,6 +24,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
@@ -49,6 +50,12 @@ class GameFragment : Fragment() {
                 container,
                 false
         )
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+        viewModel.word.observe(viewLifecycleOwner, Observer { newWord ->
+            binding.wordText.text = newWord
+        })
         binding.endGameButton.setOnClickListener { onEndGame() }
 
         Log.i("GameFragment", "Called ViewModelProvider.get")
