ab54e8c602e95a9bfd1f6b02ed453d7c811f0905
Qoolqas <qoolqas@gmail.com>
2021-02-01 08:31:29 +0700

    Add a game-finished event

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -9,6 +9,9 @@ class GameViewModel : ViewModel() {
     private val _score = MutableLiveData<Int>()
     val score: LiveData<Int>
         get() = _score
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish: LiveData<Boolean>
+        get() = _eventGameFinish
 
     // The current word
     private val _word = MutableLiveData<String>()
@@ -59,8 +62,10 @@ class GameViewModel : ViewModel() {
      * Moves to the next word in the list
      */
     private fun nextWord() {
-        if (wordList.isNotEmpty()) {
-            //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            onGameFinish()
+        } else {
+            //Select and remove a _word from the list
             _word.value = wordList.removeAt(0)
         }
     }
@@ -78,5 +83,11 @@ class GameViewModel : ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("GameViewModel", "GameViewModel destroyed!")
+    }
+    fun onGameFinish() {
+        _eventGameFinish.value = true
+    }
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
     }
 }
