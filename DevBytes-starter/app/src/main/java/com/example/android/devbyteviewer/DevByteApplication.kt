4566ba5040c43f5ee2722d6438435cb96d28a740
Qoolqas <qoolqas@gmail.com>
2021-02-09 09:42:27 +0700

    Add constraints

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/DevByteApplication.kt
@@ -17,9 +17,8 @@
 package com.example.android.devbyteviewer
 
 import android.app.Application
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
+import android.os.Build
+import androidx.work.*
 import com.example.android.devbyteviewer.work.RefreshDataWorker
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -31,11 +30,25 @@ import java.util.concurrent.TimeUnit
  * Override application to setup background work via WorkManager
  */
 class DevByteApplication : Application() {
+
     private val applicationScope = CoroutineScope(Dispatchers.Default)
     private fun setupRecurringWork() {
 //        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS).build()
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(15, TimeUnit.MINUTES)
+        val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresBatteryNotLow(true)
+                .setRequiresCharging(true)
+                .apply {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        setRequiresDeviceIdle(true)
+                    }
+                }
                 .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWorker>(1, TimeUnit.DAYS)
+                .setConstraints(constraints)
+                .build()
+
         WorkManager.getInstance().enqueueUniquePeriodicWork(
                 RefreshDataWorker.WORK_NAME,
                 ExistingPeriodicWorkPolicy.KEEP,
